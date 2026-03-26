d845ab845363e0a32930b5d78f7088cffee9abaa
jorge-lgclabs <git@lgclabs.net>
2026-03-25 22:20:28 -0400

    icon_extractor.py iterates through all icons now.

--- icon_extractor.py
+++ icon_extractor.py
@@ -14,73 +14,82 @@ def extract_icons(icon_sheet_path, output_size, threshold):
     #visible_mask = Image.fromarray((~white_mask * 255).astype(np.uint8))
     #visible_mask.show()
 
+    column_coords = []
+    row_coords = []
+
     top_pixel_row = 0
-    left_most_pixel_column = 0
-    bottom_pixel_row = 0
-    right_most_pixel_column = 0
 
-    for row_index in range(len(white_mask)):
-        for pixel_index in range(len(white_mask[row_index])):
-            if white_mask[row_index][pixel_index] == False:
-                top_pixel_row = row_index
+    bottom_pixel_row = 1
+
+    while bottom_pixel_row < len(white_mask):
+        old_top = top_pixel_row
+        left_most_pixel_column = 0
+        right_most_pixel_column = 1
+        for row_index in range(top_pixel_row, len(white_mask)): # start from the top-most row
+            for pixel_index in range(left_most_pixel_column, len(white_mask[row_index])): # go pixel-by-pixel from the left-most column to right
+                if white_mask[row_index][pixel_index] == False:  # if there is any black pixel (from an icon)
+                    top_pixel_row = row_index  # mark that row as the top of the icon data
+                    break # stop there
+            if top_pixel_row > old_top: # once the top row is found, stop looking down any further rows
+                old_top = top_pixel_row
                 break
-        if top_pixel_row != 0:
-            break
 
-    #input(f'{top_pixel_row=}')
-
-    for column_index in range(len(white_mask[0])):
-        for pixel_index in range(len(white_mask)):
-            if white_mask[pixel_index][column_index] == False:
-                left_most_pixel_column = column_index
+        for row_index in range(top_pixel_row, len(white_mask)):
+            has_false = False
+            for pixel_index in range(left_most_pixel_column, len(white_mask[row_index])):
+                if white_mask[row_index][pixel_index] == False:
+                    has_false = True
+                    continue
+            if has_false:
+                continue
+            else:
+                bottom_pixel_row = row_index
                 break
-        if left_most_pixel_column != 0:
-            break
 
-    #input(f'{left_most_pixel_column=}')
-
-    print(f'{top_pixel_row=}, {left_most_pixel_column=}')
-
-    for row_index in range(top_pixel_row, len(white_mask)):
-        has_false = False
-        for pixel_index in range(len(white_mask[row_index])):
-            if white_mask[row_index][pixel_index] == False:
-                has_false = True
-                continue
-        if has_false:
-            continue
-        else:
-            bottom_pixel_row = row_index
-            break
-
-    for column_index in range(left_most_pixel_column, len(white_mask[0])):
-        has_false = False
-        for pixel_index in range(len(white_mask)):
-            if white_mask[pixel_index][column_index] == False:
-                has_false = True
-                continue
-        if has_false:
-            continue
-        else:
-            right_most_pixel_column = column_index
-            break
-
-    print(f'{bottom_pixel_row=}, {right_most_pixel_column=}')
-
-    img.crop((left_most_pixel_column, top_pixel_row, right_most_pixel_column, bottom_pixel_row)).show()
+        #input(f'{top_pixel_row=}, {bottom_pixel_row=}')
 
 
+        while right_most_pixel_column < len(white_mask[0]):
+            old_left = left_most_pixel_column
+            for column_index in range(left_most_pixel_column, len(white_mask[0])): # start from the left-most column
+                for pixel_index in range(top_pixel_row, len(white_mask)): # go pixel-by-pixel from the top-most row to bottom
+                    if white_mask[pixel_index][column_index] == False:  # if there is any black pixel (from an icon)
+                        left_most_pixel_column = column_index  # mark that column as the left (beginning) of icon data
+                        break # and stop there
+                if left_most_pixel_column > old_left: # once the left column has been found, stop looking down further columns
+                    old_left = left_most_pixel_column
+                    break
 
-    input('stop here')
+            for column_index in range(left_most_pixel_column, len(white_mask[0])):
+                has_false = False
+                for pixel_index in range(top_pixel_row, len(white_mask)):
+                    if white_mask[pixel_index][column_index] == False:
+                        has_false = True
+                        continue
+                if has_false:
+                    continue
+                else:
+                    right_most_pixel_column = column_index
+                    break
 
-    for index in range(len(white_mask[top_pixel_row])):
-        white_mask[top_pixel_row][index] = False
+            if left_most_pixel_column == right_most_pixel_column:
+                break
 
-    for index in range(len(white_mask)):
-        white_mask[index][left_most_pixel_column] = False
+            #input(f'{top_pixel_row=}, {bottom_pixel_row=}, {left_most_pixel_column=}, {right_most_pixel_column=}')
+            img.crop((left_most_pixel_column, top_pixel_row, right_most_pixel_column, bottom_pixel_row)).show()
 
-    visible_mask = Image.fromarray((~white_mask * 255).astype(np.uint8))
-    visible_mask.show()
+            left_most_pixel_column = right_most_pixel_column + 1
+
+        top_pixel_row = bottom_pixel_row + 1
+
+    # for index in range(len(white_mask[top_pixel_row])):
+    #     white_mask[top_pixel_row][index] = False
+    #
+    # for index in range(len(white_mask)):
+    #     white_mask[index][left_most_pixel_column] = False
+    #
+    # visible_mask = Image.fromarray((~white_mask * 255).astype(np.uint8))
+    # visible_mask.show()
 
     input('stop')
 
