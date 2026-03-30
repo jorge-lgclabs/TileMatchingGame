ebe100717c736d146063514b33979cf28c31740b
jorge-lgclabs <git@lgclabs.net>
2026-03-30 16:05:08 -0400

    Temporary fix for web deploy issue.

--- testing.py
+++ testing.py
@@ -257,18 +257,20 @@ class TileGame(ft.Container):
         print(self.assets_dir)
 
         dir_numbers = []
-        with os.scandir(self.assets_dir) as dir:
-            for entry in dir:
-                if 'tiles_' in entry.name:
-                    dir_numbers.append(int(entry.name.strip('tiles_')))
+        try:
+            with os.scandir(self.assets_dir) as dir:
+                for entry in dir:
+                    if 'tiles_' in entry.name:
+                        dir_numbers.append(int(entry.name.strip('tiles_')))
 
-        set_num = random.sample(dir_numbers, 1)[0]
+            set_num = random.sample(dir_numbers, 1)[0]
 
-        with os.scandir(self.assets_dir / f'tiles_{set_num}') as tile_dir:
-            for count, entry in enumerate(tile_dir):
-                tile_num = count
-
-        return set_num, tile_num
+            with os.scandir(self.assets_dir / f'tiles_{set_num}') as tile_dir:
+                for count, entry in enumerate(tile_dir):
+                    tile_num = count
+            return set_num, tile_num
+        except FileNotFoundError:
+            return 1, 64
 
 
 def get_assets_dir() -> Path:
