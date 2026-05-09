bc5c5ecce49f37202414fa0efa313bb8f4eeb14a
jorge-lgclabs <git@lgclabs.net>
2026-05-09 15:36:34 -0400

    added __init__.py to classes folder

--- testing.py
+++ testing.py
@@ -1,10 +1,8 @@
-
 import json
-
 import flet as ft
+import app.classes.TileGame
 
 def main(page: ft.Page):
-
 
     with open('tile_index.json', 'r') as file:
         tile_index = json.load(file)
@@ -13,8 +11,11 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(ft.Column(controls=[
-        TileGame(tile_index=tile_index)
-    ], alignment = ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER))
+                            TileGame(tile_index=tile_index)
+                        ],
+                        alignment = ft.MainAxisAlignment.CENTER,
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+                        )
 
 
 ft.run(main, assets_dir='assets')
